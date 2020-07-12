a4d4f3d497c689bc9c7e59f89b32281e511eede0
rizz28 <rishwan25s@gmail.com>
2020-07-12 21:45:09 +0530

    init

---
+++ train.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import plotly
+import plotly.express as px
+import plotly.offline as pyo
+import cufflinks as cf
+cf.go_offline()
+iris = pd.read_csv("W:\Iris\Iris.csv")
+print(iris.head())
